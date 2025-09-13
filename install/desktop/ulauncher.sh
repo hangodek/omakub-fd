53fbb7140b426ca860b98393c70455b5a5f89bbb
Han <icyfarhan@gmail.com>
2025-09-14 06:04:46 +0700

    Set ulauncher position to top instead of center

--- install/desktop/ulauncher.sh
+++ install/desktop/ulauncher.sh
@@ -3,6 +3,9 @@
 # Install Ulauncher via DNF
 sudo dnf install -y ulauncher
 
+# Change Ulauncher window position from center to top before first launch
+sudo sed -i 's/<property name="window_position">center<\/property>/<property name="window_position">top<\/property>/g' /usr/share/ulauncher/ui/UlauncherWindow.ui
+
 # Start ulauncher to have it populate config before we overwrite
 mkdir -p ~/.config/autostart/
 cp ~/.local/share/omakub/configs/ulauncher.desktop ~/.config/autostart/ulauncher.desktop
