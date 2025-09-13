440663e4daed54b6e9161d7d33cf759f5e2be6d0
han <icyfarhan@gmail.com>
2025-09-13 20:52:44 +0700

    The script will only support fedora 42+

--- boot.sh
+++ boot.sh
@@ -11,7 +11,7 @@ ascii_art='________                  __        ___.
 '
 
 echo -e "$ascii_art"
-echo "=> Omakub is for fresh Fedora 40+ installations only!"
+echo "=> Omakub is for fresh Fedora 42+ installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
 sudo dnf update -y >/dev/null
--- install/check-version.sh
+++ install/check-version.sh
@@ -8,11 +8,11 @@ fi
 
 . /etc/os-release
 
-# Check if running on Fedora 40 or higher
-if [ "$ID" != "fedora" ] || [ $(echo "$VERSION_ID >= 40" | bc) != 1 ]; then
+# Check if running on Fedora 42 or higher
+if [ "$ID" != "fedora" ] || [ $(echo "$VERSION_ID >= 42" | bc) != 1 ]; then
   echo "$(tput setaf 1)Error: OS requirement not met"
   echo "You are currently running: $ID $VERSION_ID"
-  echo "OS required: Fedora 40 or higher"
+  echo "OS required: Fedora 42 or higher"
   echo "Installation stopped."
   exit 1
 fi
