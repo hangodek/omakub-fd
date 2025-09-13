2192244ab830f282a88605624d24a71d5100edf9
han <icyfarhan@gmail.com>
2025-09-13 23:19:22 +0700

    Enable docker immediately after install

--- install/terminal/docker.sh
+++ install/terminal/docker.sh
@@ -10,5 +10,9 @@ sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 # Give this user privileged Docker access
 sudo usermod -aG docker ${USER}
 
+# Start and enable Docker service
+sudo systemctl start docker
+sudo systemctl enable docker
+
 # Limit log size to avoid running out of disk
 echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
