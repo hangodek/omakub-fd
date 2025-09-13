a0815058a9d65dff6a29b824ed8b3497ea91f023
Han <icyfarhan@gmail.com>
2025-09-14 00:51:13 +0700

    Install ulauncher from dnf instead of flathub

--- install/desktop/ulauncher.sh
+++ install/desktop/ulauncher.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Install Ulauncher via Flatpak for better Fedora compatibility
-flatpak install -y flathub io.ulauncher.Ulauncher
+# Install Ulauncher via DNF
+sudo dnf install -y ulauncher
 
 # Start ulauncher to have it populate config before we overwrite
 mkdir -p ~/.config/autostart/
 cp ~/.local/share/omakub/configs/ulauncher.desktop ~/.config/autostart/ulauncher.desktop
-flatpak run io.ulauncher.Ulauncher >/dev/null 2>&1 &
+ulauncher >/dev/null 2>&1 &
 sleep 2 # ensure enough time for ulauncher to set defaults
 cp ~/.local/share/omakub/configs/ulauncher.json ~/.config/ulauncher/settings.json
