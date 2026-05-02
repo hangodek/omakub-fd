c1fbb370220f10b6672d5cd876969b02d20b492c
Muhammad Farhan Pulungan <icyfarhan@gmail.com>
2026-05-03 01:51:17 +0700

    feat(desktop): revert Caps Lock to default behavior
    - Removed the compose key mapping from Caps Lock so it functions as a normal uppercase toggle

--- install/desktop/set-xcompose.sh
+++ install/desktop/set-xcompose.sh
@@ -2,4 +2,5 @@
 
 envsubst < ~/.local/share/omakub/configs/xcompose > ~/.XCompose
 ibus restart
-gsettings set org.gnome.desktop.input-sources xkb-options "['compose:caps']"
+# Keep Caps Lock as normal Caps Lock instead of mapping it to compose key for emojis
+gsettings set org.gnome.desktop.input-sources xkb-options "[]"
