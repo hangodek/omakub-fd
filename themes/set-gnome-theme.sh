68dd6a4f1195ac1497cbfc9f60482eea8a7871cd
Muhammad Farhan Pulungan <icyfarhan@gmail.com>
2026-05-04 21:21:55 +0700

    style(desktop): set default GNOME theme to Adwaita
    - Changed cursor-theme, gtk-theme, and icon-theme to 'Adwaita' (default GNOME style) instead of Yaru

--- themes/set-gnome-theme.sh
+++ themes/set-gnome-theme.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
-gsettings set org.gnome.desktop.interface gtk-theme "Yaru-$OMAKUB_THEME_COLOR-dark"
-gsettings set org.gnome.desktop.interface icon-theme "Yaru-$OMAKUB_THEME_COLOR"
+gsettings set org.gnome.desktop.interface cursor-theme 'Adwaita'
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
+gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'
 gsettings set org.gnome.desktop.interface accent-color "$OMAKUB_THEME_COLOR" 2>/dev/null || true
 
 BACKGROUND_ORG_PATH="$HOME/.local/share/omakub/themes/$OMAKUB_THEME_BACKGROUND"
