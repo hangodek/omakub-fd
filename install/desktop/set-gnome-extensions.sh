da36bbba4d72f7a029cee6ec674392efcceb6fc6
han <icyfarhan@gmail.com>
2026-05-12 19:17:53 +0700

    Update GNOME extensions setup

--- install/desktop/set-gnome-extensions.sh
+++ install/desktop/set-gnome-extensions.sh
@@ -16,16 +16,17 @@ gum confirm "To install Gnome extensions, you need to accept some confirmations.
 
 # Install user extensions
 gext install tactile@lundal.io
-gext install space-bar@luchrioh
+gext install just-perfection-desktop@just-perfection
 gext install undecorate@sun.wxg@gmail.com
 gext install AlphabeticalAppGrid@stuarthayhurst
 gext install caffeine@patapon.info
-gext install impatience@gfxmonk.net
+gext install Vitals@CoreCoding.com
 
 # Compile gsettings schemas in order to be able to set them
 sudo cp ~/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml /usr/share/glib-2.0/schemas/ 2>/dev/null || true
-sudo cp ~/.local/share/gnome-shell/extensions/space-bar\@luchrioh/schemas/org.gnome.shell.extensions.space-bar.gschema.xml /usr/share/glib-2.0/schemas/ 2>/dev/null || true
+sudo cp ~/.local/share/gnome-shell/extensions/just-perfection-desktop\@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml /usr/share/glib-2.0/schemas/ 2>/dev/null || true
 sudo cp ~/.local/share/gnome-shell/extensions/AlphabeticalAppGrid\@stuarthayhurst/schemas/org.gnome.shell.extensions.AlphabeticalAppGrid.gschema.xml /usr/share/glib-2.0/schemas/ 2>/dev/null || true
+sudo cp ~/.local/share/gnome-shell/extensions/Vitals\@CoreCoding.com/schemas/org.gnome.shell.extensions.vitals.gschema.xml /usr/share/glib-2.0/schemas/ 2>/dev/null || true
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
 
 has_schema() {
@@ -52,6 +53,12 @@ set_gsetting org.gnome.shell.extensions.tactile col-3 0
 set_gsetting org.gnome.shell.extensions.tactile row-0 1
 set_gsetting org.gnome.shell.extensions.tactile row-1 1
 set_gsetting org.gnome.shell.extensions.tactile gap-size 32
+
+# Configure Just Perfection
+set_gsetting org.gnome.shell.extensions.just-perfection animation 0
+set_gsetting org.gnome.shell.extensions.just-perfection dash-app-running true
+set_gsetting org.gnome.shell.extensions.just-perfection workspace true
+set_gsetting org.gnome.shell.extensions.just-perfection workspace-popup false
 
 # Configure AlphabeticalAppGrid
 set_gsetting org.gnome.shell.extensions.alphabetical-app-grid folder-order-position "end"
