dd543041a458c3cc0c4cad9db149b4526db529c2
han <icyfarhan@gmail.com>
2026-05-12 17:44:36 +0700

    Default theme to catpuccin

--- install/desktop/set-gnome-theme.sh
+++ install/desktop/set-gnome-theme.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source ~/.local/share/omakub/themes/tokyo-night/gnome.sh
-source ~/.local/share/omakub/themes/tokyo-night/tophat.sh
+source ~/.local/share/omakub/themes/catppuccin/gnome.sh
+source ~/.local/share/omakub/themes/catppuccin/tophat.sh
