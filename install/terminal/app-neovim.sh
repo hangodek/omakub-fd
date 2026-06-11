3544f29569612821e93f1b1e387451c807c677c5
han <icyfarhan@gmail.com>
2026-06-11 21:41:35 +0700

    Fixed neovim doesnt installed properly

--- install/terminal/app-neovim.sh
+++ install/terminal/app-neovim.sh
@@ -13,3 +13,38 @@ if ! command -v nvim &> /dev/null; then
 else
   echo "Neovim is already installed, skipping."
 fi
+
+# Install helper tools to reduce LazyVim :checkhealth warnings
+sudo dnf install -y luarocks tree-sitter-cli
+
+# Only attempt to set configuration if Neovim has never been run
+if [ ! -d "$HOME/.config/nvim" ]; then
+  # Use LazyVim
+  git clone https://github.com/LazyVim/starter ~/.config/nvim
+  # Remove the .git folder, so you can add it to your own repo later
+  rm -rf ~/.config/nvim/.git
+
+  # Make everything match the terminal transparency
+  mkdir -p ~/.config/nvim/plugin/after
+  cp ~/.local/share/omakub/configs/neovim/transparency.lua ~/.config/nvim/plugin/after/
+
+  # Default to Tokyo Night theme
+  mkdir -p ~/.config/nvim/lua/plugins
+  cp ~/.local/share/omakub/themes/tokyo-night/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
+
+  # Turn off animated scrolling
+  cp ~/.local/share/omakub/configs/neovim/snacks-animated-scrolling-off.lua ~/.config/nvim/lua/plugins/
+
+  # Turn off relative line numbers
+  echo "vim.opt.relativenumber = false" >> ~/.config/nvim/lua/config/options.lua
+
+  # Ensure editor.neo-tree is used by default
+  cp ~/.local/share/omakub/configs/neovim/lazyvim.json ~/.config/nvim/
+fi
+
+# Replace desktop launcher with one running inside Alacritty
+if [[ -d ~/.local/share/applications ]]; then
+  sudo rm -rf /usr/share/applications/nvim.desktop
+  sudo rm -rf /usr/local/share/applications/nvim.desktop
+  source ~/.local/share/omakub/applications/Neovim.sh
+fi
